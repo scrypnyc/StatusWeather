5be41990cd9536ede12974de7591ace6633e9a82
scrypnyc <alexanderlookua@gmail.com>
2019-05-07 14:36:14 +0300

    add change

--- WeatherApp/Controller/ViewController.swift
+++ WeatherApp/Controller/ViewController.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 import Alamofire
 import SwiftyJSON
+import NVActivityIndicatorView
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
@@ -30,11 +31,26 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
   let apiKey = "762a34b466f8a3678e4f1717f1d9aaa2"
   var latitude = 2.33
   var longitude = 20.22
+  var activityIndicator: NVActivityIndicatorView!
   let locationManager = CLLocationManager()
   
   override func viewDidLoad() {
         super.viewDidLoad()
         backgroundView.layer.addSublayer(gradientLayer)
+    
+    let indicatorSize: CGFloat = 70
+    let indicatorFrame = CGRect(x: (view.frame.width-indicatorSize)/2, y: (view.frame.height-indicatorSize)/2, width: indicatorSize, height: indicatorSize)
+    activityIndicator = NVActivityIndicatorView(frame: indicatorFrame, type: .lineScale, color: UIColor.clear, padding: 20.0)
+    view.addSubview(activityIndicator)
+    
+    locationManager.requestWhenInUseAuthorization()
+    
+    activityIndicator.startAnimating()
+    if(CLLocationManager.locationServicesEnabled()) {
+      locationManager.delegate = self
+      locationManager.desiredAccuracy = kCLLocationAccuracyBest
+      locationManager.startUpdatingLocation()
+    }
   }
   
   override func viewWillAppear(_ animated: Bool) {
@@ -42,12 +58,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
   }
   
   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-  let location = locations[0]
+    let location = locations[0]
     latitude = location.coordinate.latitude
     longitude = location.coordinate.longitude
-  Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&units=metric").responseJSON { response in
+    Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&units=metric").responseJSON { response in
+      self.activityIndicator.stopAnimating()
       
-  if let responseStr = response.result.value {
+      if let responseStr = response.result.value {
         let jsonResponse = JSON(responseStr)
         let jsonWeather = jsonResponse["weather"].array![0]
         let jsonTemp = jsonResponse["main"]
@@ -79,15 +96,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
   }
   
   func setLightBackground() {
-    let topColor = UIColor.purple.cgColor
-    let bottomColor = UIColor.purple.cgColor
+    let topColor = UIColor.darkGray.cgColor
+    let bottomColor = UIColor.lightGray.cgColor
     gradientLayer.frame = view.bounds
     gradientLayer.colors = [topColor, bottomColor]
   }
 
   func setDarkBackground() {
     let topColor = UIColor.black.cgColor
-    let bottomColor = UIColor.purple.cgColor
+    let bottomColor = UIColor.darkGray.cgColor
     gradientLayer.frame = view.bounds
     gradientLayer.colors = [topColor, bottomColor]
   }
