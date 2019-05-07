155b756c1e819434ec37027b12a3d96157338c80
scrypnyc <alexanderlookua@gmail.com>
2019-05-07 14:20:09 +0300

    Activity Indicator
    Delete

--- WeatherApp/Controller/ViewController.swift
+++ WeatherApp/Controller/ViewController.swift
@@ -10,44 +10,31 @@ import Foundation
 import UIKit
 import Alamofire
 import SwiftyJSON
-import NVActivityIndicatorView
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
   @IBOutlet weak var locationLabel: UILabel!
   @IBOutlet weak var dayLabel: UILabel!
+  
   @IBOutlet weak var imageView: UIImageView!
   @IBOutlet weak var conditionLabel: UILabel!
+  
   @IBOutlet weak var temperatureLabel: UILabel!
   @IBOutlet weak var backgroundView: UIView!
+  
+  
   
   let gradientLayer = CAGradientLayer()
   
   let apiKey = "762a34b466f8a3678e4f1717f1d9aaa2"
   var latitude = 2.33
   var longitude = 20.22
-  var activityIndicator: NVActivityIndicatorView!
   let locationManager = CLLocationManager()
   
   override func viewDidLoad() {
         super.viewDidLoad()
         backgroundView.layer.addSublayer(gradientLayer)
-    
-    let indicatorSize: CGFloat = 70
-    let indicatorFrame = CGRect(x: (view.frame.width-indicatorSize)/2, y: (view.frame.height-indicatorSize)/2, width: indicatorSize, height: indicatorSize)
-    activityIndicator = NVActivityIndicatorView(frame: indicatorFrame, type: .lineScale, color: UIColor.white, padding: 20.0)
-    activityIndicator.backgroundColor = UIColor.black
-    view.addSubview(activityIndicator)
-    
-    locationManager.requestWhenInUseAuthorization()
-    
-    activityIndicator.startAnimating()
-    if(CLLocationManager.locationServicesEnabled()) {
-      locationManager.delegate = self
-      locationManager.desiredAccuracy = kCLLocationAccuracyBest
-      locationManager.startUpdatingLocation()
-    }
   }
   
   override func viewWillAppear(_ animated: Bool) {
@@ -55,12 +42,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
   }
   
   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-    let location = locations[0]
+  let location = locations[0]
     latitude = location.coordinate.latitude
     longitude = location.coordinate.longitude
-    Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&units=metric").responseJSON { response in
-      self.activityIndicator.stopAnimating()
-      if let responseStr = response.result.value {
+  Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&units=metric").responseJSON { response in
+      
+  if let responseStr = response.result.value {
         let jsonResponse = JSON(responseStr)
         let jsonWeather = jsonResponse["weather"].array![0]
         let jsonTemp = jsonResponse["main"]
